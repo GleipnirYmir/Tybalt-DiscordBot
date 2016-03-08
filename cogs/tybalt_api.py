fd3ff4bae9674edff8fe97257f52872f04fdd1e3
Florian Grasset <florian.grasset@gmail.com>
2016-03-08 14:20:49 +0100

    Adding Tybalt API

---
+++ cogs/tybalt_api.py
@@ -0,0 +1,28 @@
+import discord
+import subprocess
+from discord.ext import commands
+from .utils import checks
+from __main__ import user_allowed, send_cmd_help
+import os
+
+class TybaltApi:
+    """TybaltApi."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.api_paths = fileIO("data/tybalt/api.json", "load")
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def skill(self, ctx, skill:str):
+        """Describe a skill
+
+        Example:
+        !skill Fireball
+        """
+        path = self.api_paths['skill'];
+        response = subprocess.check_output(["php", path, skill]);
+        await self.bot.say(response);
+
+def setup(bot):
+    n = TybaltMegaserver(bot)
+    bot.add_cog(n)
